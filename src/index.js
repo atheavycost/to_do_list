637887072121f575abb5603a6704edaf9fae16d6
Alex <ritterod@gmail.com>
2022-11-17 14:16:13 +0400

    psinus prolactinus, frist commitus.

--- src/index.js
+++ src/index.js
@@ -1,17 +1,19 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import ReactDOM from 'react-dom';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+const el = (
+    <div>
+        <h1> My todo list</h1>
+        <input placeholder='search' />
+        <ul>
+            <li>Learn React</li>
+            <li>Build Awesome App</li>
+        </ul>
+
+
+    </div>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// const el = React.createElement('h1', null, 'Hello world epta');
+
+ReactDOM.render(el, document.getElementById('root'));
