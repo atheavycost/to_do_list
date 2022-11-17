8146319f8c2acb78528bcf799db897900736500d
Alex <ritterod@gmail.com>
2022-11-17 14:59:09 +0400

    создание реакт-компонентов.

--- src/index.js
+++ src/index.js
@@ -1,20 +1,40 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-const el = (
-    <div>
+const Todolist = () => {
+    return (        
+    <ul>
+        <li>Learn React</li>
+        <li>Build Awesome App</li>
+        <li>To do something</li>
+    </ul>
+    );
+};
+
+const Appheader = () => {
+    return (
         <h1> My todo list</h1>
+);};
+
+const SearchPanel = () => {
+    return (
         <input placeholder='search' />
-        <ul>
-            <li>Learn React</li>
-            <li>Build Awesome App</li>
-            <li>To do something</li>
-        </ul>
+    );
+};
 
+const App = () => {
+    return (
+        <div>
+        <Appheader />
+        <SearchPanel />
+        <Todolist />
+    </div>        
+    );
+};
 
-    </div>
-);
+// const el = <App />;
 
 // const el = React.createElement('h1', null, 'Hello world epta');
 
-ReactDOM.render(el, document.getElementById('root'));
+// ReactDOM.render(el, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'));
