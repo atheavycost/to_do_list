d40d0663256e3cc1715caae504b3c10cd048dd6c
atheavycost <maslowik@yandex.ru>
2022-11-21 13:02:28 +0400

    todo-list-item создан класс TodoListItem

--- src/components/todo-list-item/todo-list-item.js
+++ src/components/todo-list-item/todo-list-item.js
@@ -1,17 +1,40 @@
 import React from 'react';
 import './todo-list-item';
 
-const TodoListItem = ({label, important = false}) => {
 
-    const style = {
-        color: important ? 'tomato' : 'blue'
-    };
-    return (
-        <span className='todo-list-item' 
-              style={style}>
-            { label }
-        </span>
-    );
-};
+class TodoListItem extends React.Component {
+
+    render() {
+        const {label, important = false} = this.props;
+
+        const style = {
+                color: important ? 'tomato' : 'blue',
+                fontWeight: important ? 'bold' : 'normal'
+            };
+
+        return (
+            <span className='todo-list-item'>
+                <span 
+                    className='todo-list-item-label'
+                    style={style}>
+                    { label }
+                </span>
+        
+                <button type='button'
+                        className='btn btn-out-line-success btn-sm float-right'>
+                    <li className='fa fa-exclamation' />
+                </button>
+        
+                <button type='button'
+                        className='btn btn-out-line-danger btn-sm float-right'>
+                    <li className='fa fa-trash-o' />
+                </button>
+
+            </span>
+        
+            );
+        };
+    
+    }
 
 export default TodoListItem;
