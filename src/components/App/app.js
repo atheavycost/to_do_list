c6683f38cfdfc80f80286a649b46d46b6d8b15fc
atheavycost <maslowik@yandex.ru>
2022-11-22 13:37:42 +0400

    новый компонент кнопка добавления такска

--- src/components/App/app.js
+++ src/components/App/app.js
@@ -4,6 +4,7 @@ import AppHeader from '../app-header';
 import SearchPanel from '../search-panel';
 import TodoList from '../todo-list/todo-list';
 import ItemStatusFilter from '../item-status-filter';
+import ItemAddForm from '../item-add-form';
 
 import './app.css';
 
@@ -44,6 +45,10 @@ export default class App extends Component {
                 <TodoList 
                 todos={this.state.todoData} 
                 onDeleted={this.deletItem}/>
+                <div>
+                    <ItemAddForm />
+                </div>
+                
             </div>        
         );
     };
