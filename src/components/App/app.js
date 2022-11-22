50643d06fdc3b4e80fdaf888351503170c44f469
atheavycost <maslowik@yandex.ru>
2022-11-22 12:51:37 +0400

    добавил состояние для данных в app.js

--- src/components/App/app.js
+++ src/components/App/app.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {Component} from 'react';
 
 import AppHeader from '../app-header';
 import SearchPanel from '../search-panel';
@@ -7,26 +7,31 @@ import ItemStatusFilter from '../item-status-filter';
 
 import './app.css';
 
-const App = () => {
+export default class App extends Component {
 
-    const todoDate = [
-        { label: 'Drink Coffee', important: false, id: 1},
-        { label: 'make awesome app', important: true, id: 2},
-        { label: 'Drink VODKA', important: true, id: 3}
-    ];
+    state = {
+        todoData: [
+            { label: 'Drink Coffee', important: false, id: 1},
+            { label: 'make awesome app', important: true, id: 2},
+            { label: 'Drink VODKA', important: true, id: 3}
+        ]
+        
+    }
     
-    return (
-        <div className='todo-app'>
-            <AppHeader toDo={10} done={3}/>
-            <div className='search-panel d-flex'>
-                <SearchPanel />
-                <ItemStatusFilter/>
-            </div>
+    render() {
+        return (
+            <div className='todo-app'>
+                <AppHeader toDo={10} done={3}/>
+                <div className='search-panel d-flex'>
+                    <SearchPanel />
+                    <ItemStatusFilter/>
+                </div>
+    
+                <TodoList 
+                todos={this.state.todoData} 
+                onDeleted={(id) => console.log('deleted', id)}/>
+            </div>        
+        );
+    };
+    }
 
-            <TodoList todos={todoDate} 
-            onDeleted={(id) => console.log('deleted', id)}/>
-        </div>        
-    );
-};
-
-export default App;
